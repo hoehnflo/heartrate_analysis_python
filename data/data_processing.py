d591d3aaa720e30c3c26e45c44e7d805ae6bd04d
Florian Höhn <hoehnflo@students.zhaw.ch>
2023-12-04 13:20:24 +0100

    123

--- data/data_processing.py
+++ data/data_processing.py
@@ -1,6 +1,6 @@
 import heartpy as hp
 import matplotlib.pyplot as plt
+import numpy as np
 
-hp.get_data("C:\Users\Administrator\heartrate_analysis_python\ppggreen.csv")
-data = hp.get_data("C:\Users\Administrator\heartrate_analysis_python\ppggreen.csv")
+data = hp.get_data('ppggreen.csv')
 
